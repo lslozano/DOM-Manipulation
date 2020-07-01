555336855527599632e4356da156be7df7f09dfb
Luis Lozano <lslozano11@gmail.com>
2020-07-01 17:03:40 -0500

    Added flag to show which player won
    An icon of a flag is displayed on each side depending on what player won. If no player wins, the flag is not displayed.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ let dice2 = document.querySelector(".img2");
 // Variable that represents the h1 title.
 let decision = document.querySelector("h1");
 
+// Variables that represents the winner flag.
+let playerOneWins = document.querySelector(".playerOneWins");
+let playerTwoWins = document.querySelector(".playerTwoWins");
+
 // Setting the src of the image dynamically according to roll value.
 dice1.src = `images/dice${playerOneRoll}.png`;
 dice2.src = `images/dice${playerTwoRoll}.png`;
@@ -20,8 +24,10 @@ dice2.src = `images/dice${playerTwoRoll}.png`;
 // Deciding winner or if it is a tie and setting the text of the h1 accordingly.
 if (playerOneRoll > playerTwoRoll) {
   decision.innerText = "Player 1 Wins!";
+  playerOneWins.style.visibility = "visible";
 } else if (playerOneRoll < playerTwoRoll) {
   decision.innerText = "Player 2 Wins!";
+  playerTwoWins.style.visibility = "visible";
 } else if (playerOneRoll == playerTwoRoll) {
   decision.innerText = "Draw!";
 }
