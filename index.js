d5964295ee10f2467517880f048fec6a24b1abb6
Luis Lozano <lslozano11@gmail.com>
2020-07-01 16:21:08 -0500

    Dice rolls and image selected ready

---
+++ index.js
@@ -0,0 +1,8 @@
+// Dice rolls.
+const playerOneRoll = Math.floor((Math.random() * 6) + 1);
+const playerTwoRoll = Math.floor((Math.random() * 6) + 1);
+
+// Constants that represent the Dice images.
+const dice1 = document.querySelector(".img1");
+const dice2 = document.querySelector(".img2");
+
