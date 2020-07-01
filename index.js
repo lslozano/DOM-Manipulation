a34d4b8b1c1bfdab62569f63192f923e7a3514ce
Luis Lozano <lslozano11@gmail.com>
2020-07-01 16:25:46 -0500

    Image source set dynamically

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const playerOneRoll = Math.floor((Math.random() * 6) + 1);
 const playerTwoRoll = Math.floor((Math.random() * 6) + 1);
 
 // Constants that represent the Dice images.
-const dice1 = document.querySelector(".img1");
-const dice2 = document.querySelector(".img2");
+let dice1 = document.querySelector(".img1");
+let dice2 = document.querySelector(".img2");
+
+// Setting the src of the image dynamically according to roll value.
+dice1.src = `images/dice${playerOneRoll}.png`;
+dice2.src = `images/dice${playerTwoRoll}.png`;
 
